1bcb1b6de942dcc554821833ee6dc3439e4c1106
Hisham <hmuhammed546@gmail.com>
2023-10-13 12:08:02 +0530

    implmented crud operations in frontend

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -5,15 +5,16 @@ import BackButton from '../components/BackButton';
 import { Spinner } from '../components/Spinner';
 
 const ShowBook = () => {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState({});
   const [loading, setLoading] = useState(false);
-  const {id} =useParams();
+  const { id } = useParams();
+  
   useEffect(() => {
     setLoading(true);
     axios.get(`http://localhost:5555/books/${id}`)
       .then((response) => {
         console.log(response)
-        setBooks(response.data.data);
+        setBooks(response.data);
         setLoading(false)
       }).catch((error) => {
         setLoading(false);
@@ -33,27 +34,27 @@ const ShowBook = () => {
         <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Id</span>
-            <span>{books._id}</span>
+            <span>{books?._id}</span>
           </div>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Title</span>
-            <span>{books.title}</span>
+            <span>{books?.title}</span>
           </div>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Author</span>
-            <span>{books.author}</span>
+            <span>{books?.author}</span>
           </div>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
-            <span>{books.publishYear}</span>
+            <span>{books?.publishYear}</span>
           </div>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Create Time</span>
-            <span>{new Date(books.createdAt).toString()}</span>
+            <span>{new Date(books?.createdAt).toString()}</span>
           </div>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
-            <span>{new Date(books.updatedAt).toString()}</span>
+            <span>{new Date(books?.updatedAt).toString()}</span>
           </div>
         </div>
       )}
